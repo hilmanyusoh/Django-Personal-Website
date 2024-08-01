8f223791db33d289903154625b479eec07aa4fa1
Hilman Yusoh <118374893+hilmanyusoh@users.noreply.github.com>
2024-08-01 09:01:50 +0700

    Updata

--- personalweb/myproject/myproject/settings.py
+++ personalweb/myproject/myproject/settings.py
@@ -54,8 +54,7 @@ ROOT_URLCONF = "myproject.urls"
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"], 
+        "BACKEND": "django.template.backends.django.DjangoTemplates", 
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -118,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
