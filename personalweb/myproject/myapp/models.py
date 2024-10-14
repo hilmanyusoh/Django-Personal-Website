ded45d9a1915af8559b8c3aece72075e6d0c9a2b
Hilman Yusoh <118374893+hilmanyusoh@users.noreply.github.com>
2024-10-14 14:00:04 +0700

    .

--- personalweb/myproject/myapp/models.py
+++ personalweb/myproject/myapp/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-class Person(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.IntegerField()  # Field for storing age as an integer
-    date = models.DateField()    # Field for storing date
+class AcademicProject(models.Model):
+    title = models.CharField(max_length=200, help_text="Enter the project title")
+    description = models.TextField(help_text="Enter the project description")
+    date_posted = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return "ประวัติส่วนตัวของ"+ self.name
+        return self.title
--- personalweb/myproject/myapp/views.py
+++ personalweb/myproject/myapp/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import AcademicProject
 
-# Create your views here.
 def index(request):
-    return render(request,"index.html")
-
+    academic_projects = AcademicProject.objects.all().order_by('-date_posted')
+    return render(request, "index.html", {'academic_projects': academic_projects})
 
 
 
