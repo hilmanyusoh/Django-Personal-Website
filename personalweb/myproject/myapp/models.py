a1e49397558a633a1d38a44fdd3f3412f43e92e6
Hilman Yusoh <118374893+hilmanyusoh@users.noreply.github.com>
2024-10-14 15:19:40 +0700

    Update

--- personalweb/myproject/myapp/models.py
+++ personalweb/myproject/myapp/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
 class AcademicProject(models.Model):
-    title = models.CharField(max_length=200, help_text="Enter the project title")
-    description = models.TextField(help_text="Enter the project description")
-    date_posted = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
 
     def __str__(self):
         return self.title
--- personalweb/myproject/myapp/views.py
+++ personalweb/myproject/myapp/views.py
@@ -2,10 +2,8 @@ from django.shortcuts import render
 from .models import AcademicProject
 
 def index(request):
-    academic_projects = AcademicProject.objects.all().order_by('-date_posted')
-    return render(request, "index.html", {'academic_projects': academic_projects})
-
-
+    academic_projects = AcademicProject.objects.all()
+    return render(request, 'index.html', {'academic_projects': academic_projects})
 
 
 
